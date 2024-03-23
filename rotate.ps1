62e13c92044440b286005ef52ff3d6c4ef6ce684
Martynas <martitis9@gmail.com>
2024-03-23 21:02:26 +0200

    Add files via upload

---
+++ rotate.ps1
@@ -0,0 +1,10 @@
+$directory = "$env:USERPROFILE\Pictures\Screenshots"
+$newestImage = Get-ChildItem -Path $directory -Filter *.png | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+
+if ($newestImage -ne $null) {
+    $image = [System.Drawing.Image]::FromFile($newestImage.FullName)
+    $image.RotateFlip([System.Drawing.RotateFlipType]::Rotate180FlipNone)
+    $newImagePath = Join-Path -Path $directory -ChildPath "rotated_image.png"
+    $image.Save($newImagePath)
+    $image.Dispose()
+}
